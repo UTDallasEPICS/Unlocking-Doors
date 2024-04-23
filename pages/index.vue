aec98d6207415caf61573b661f11d022f6f6e9e8
phegde19 <156964650+phegde19@users.noreply.github.com>
2024-04-23 15:17:36 -0500

    UI Changes

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,7 @@
                 td #[strong PHONE] 
                   br
                   br
-                  |{{ contact.mainPhone ? contact.mainPhone : (contact.directPhone ? contact.directPhone : contact.cellPhone) }}
+                  |{{ contact.mainPhone ? contact.mainPhone : (contact.directPhone ? contact.directPhone : contact.mobilePhone) }}
                 td {{ contact.company ? contact.company : &apos;&apos; }}
           .pagination
             button(@click="prevPage()") Previous
