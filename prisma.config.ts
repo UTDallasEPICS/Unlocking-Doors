fbe4179a01111b89017da4500dddfcce0d022fc2
Vikas Thoutam <vikas.thoutam@gmail.com>
2026-08-12 20:46:50 -0500

    chore: push build

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,7 @@
-import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -8,6 +10,6 @@ export default defineConfig({
     seed: "npx tsx prisma/seed.ts",
   },
   datasource: {
-    url: env("DATABASE_URL") ?? "file:./dev.db",
+      url: process.env.DATABASE_URL || 'file:./prisma/dev.db',
   },
 });
